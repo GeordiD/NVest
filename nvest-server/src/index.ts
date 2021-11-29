d66fb3db81c82f5f65c958e56e233dc1d64ddd30
Geordi Dosher <geordi.dosher@gmail.com>
2021-11-28 16:29:07 -0800

    Updated docker build
    * Nodemon now works while up
    * Debugging now works

--- nvest-server/src/index.ts
+++ nvest-server/src/index.ts
@@ -5,8 +5,8 @@ const app = express();
 const port = 5000;
 
 app.get('/', async (_, res) => {
-    const result = await new AuthService().saveAccessToken();
-    res.status(200).send(result);
+    // const result = await new AuthService().saveAccessToken();
+    res.status(200).send('b r o n c o s');
 });
 
 app.listen(port, () => console.log(`Running on port ${port}`));
