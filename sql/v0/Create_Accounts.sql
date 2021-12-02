cb0fe3cd8acbc11998c12615f7eeace777570b0d
Geordi Dosher <geordi.dosher@gmail.com>
2021-12-01 21:53:56 -0600

    Removing postgres to switch to mongo
    After thinking about this, I want to use this as an opportunity to try out NoSql. For this project, the potential large data coming from the market could warrant it anyway

--- sql/v0/Create_Accounts.sql
+++
@@ -1,4 +0,0 @@
-create table accounts (
-	user_id serial primary key,
-	username varchar(50) unique not null
-)
--- sql/v0/Create_TdaIntegrations.sql
+++
@@ -1,5 +0,0 @@
-create table tda_integrations (
-	id serial primary key,
-	user_id varchar(50) unique not null,
-	code text not null
-)
